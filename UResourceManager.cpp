72fe165ddba4c825bf915103e222d67122b5a71e
구민상 <129085079+808Jade@users.noreply.github.com>
2026-08-31 20:46:41 +0900

    Microsoft DirectXTK 라이브러리 설치, UResourceManager 추가

--- UResourceManager.cpp
+++ UResourceManager.cpp
@@ -1,2 +1,38 @@
 #include "pch.h"
 #include "UResourceManager.h"
+
+UResourceManager::UResourceManager(ID3D11Device* device)
+	: m_device(device)
+{
+
+}
+
+UResourceManager::~UResourceManager()
+{
+
+}
+
+ID3D11ShaderResourceView* UResourceManager::GetTexture(const std::string& key)
+{
+    // 이미 로드된 텍스처면 바로 반환
+    if (m_textures.find(key) != m_textures.end())
+        return m_textures[key].Get();
+
+    std::wstring wKey(key.begin(), key.end());
+
+    // 텍스처 생성
+    HRESULT hr = DirectX::CreateWICTextureFromFile(
+        m_device.Get(),                         
+        wKey.c_str(),                           
+        nullptr,                                
+        m_textures[key].ReleaseAndGetAddressOf()
+    );
+
+    if (FAILED(hr))
+    {
+        // 로드 실패 시 에러 처리 (예: nullptr 반환 등)
+        return nullptr;
+    }
+
+    return m_textures[key].Get();
+}
--- UResourceManager.h
+++ UResourceManager.h
@@ -2,13 +2,17 @@
 
 #include "pch.h"
 
-//class UResourceManager
-//{
-//public:
-//	UResourceManager();
-//	~UResourceManager();
-//
-//private:
-//	std::unordered_map<std::string, Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>> m_textures;
-//};
-//
+class UResourceManager
+{
+public:
+	UResourceManager(ID3D11Device* device);
+	~UResourceManager();
+
+	ID3D11ShaderResourceView* GetTexture(const std::string& key);
+
+private:
+	Microsoft::WRL::ComPtr<ID3D11Device> m_device;
+
+	std::unordered_map<std::string, Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>> m_textures;
+};
+
